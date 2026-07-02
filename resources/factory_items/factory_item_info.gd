013c61e96de6a52c0a2d000bdc5a468aba3773ec
Kimbix <alemanhumberto06@gmail.com>
2026-07-02 18:25:58 -0400

    feat: Item generator outputs items on the middle of the conveyor belt now

--- resources/factory_items/factory_item_info.gd
+++ resources/factory_items/factory_item_info.gd
@@ -3,3 +3,7 @@ extends Resource
 
 @export var name: StringName = &""
 @export var texture: Texture2D
+
+var grid_size: Vector2:
+	get():
+		return texture.get_size() / PlayerGrid.GRID_TEXTURE_SIZE
--- scripts/components/item_generator.gd
+++ scripts/components/item_generator.gd
@@ -1,11 +1,19 @@
 class_name ItemGenerator
 extends FactoryComponent
 
-var generating: FactoryItemInfo
+var generating: FactoryItemInfo:
+	set(v):
+		generating = v
+		_precompute_offsets()
+		_cooldown = 0
 var _cooldown: int = 0
+var _offsets: Dictionary = { }
 
 
 func tick() -> void:
+	if generating == null:
+		return
+
 	if _cooldown > 0:
 		_cooldown -= 1
 		return
@@ -14,6 +22,15 @@ func tick() -> void:
 	var output_port := _get_available_out_port()
 	if output_port == null:
 		return
-	var where_to := position + output_port.position + output_port.facing
-	grid.place_item(generating, where_to)
+	var where_to: Vector2 = position + output_port.position + output_port.facing
+	grid.place_item(generating, where_to + _offsets[rotation])
 	_cooldown = 40
+
+
+func _precompute_offsets() -> void:
+	if generating == null:
+		return
+	_offsets[FactoryBuilding.Rotation.NORMAL] = Vector2.DOWN * .5 + Vector2.UP * generating.grid_size.y * .5
+	_offsets[FactoryBuilding.Rotation.CLOCKWISE] = Vector2.RIGHT * .5 + Vector2.LEFT * generating.grid_size.x * .5
+	_offsets[FactoryBuilding.Rotation.COUNTERCLOCKWISE] = Vector2.DOWN + Vector2.UP * generating.grid_size.y + Vector2.RIGHT * .5 + Vector2.LEFT * generating.grid_size.x * .5
+	_offsets[FactoryBuilding.Rotation.FLIPPED] = Vector2.RIGHT + Vector2.LEFT * generating.grid_size.x + Vector2.DOWN * .5 + Vector2.UP * generating.grid_size.y * .5

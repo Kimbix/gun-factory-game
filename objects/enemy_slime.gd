7f3535c78d2aaeacadcc4429b020e569cc019fab
Kimbix <alemanhumberto06@gmail.com>
2026-07-20 18:46:15 -0400

    fix: Enemy angry robot fixed

--- objects/enemy_slime.gd
+++ objects/enemy_slime.gd
@@ -1,9 +1,9 @@
 class_name EnemySlime
 extends BaseEnemy
 
-const SPEED := 60.0
-
 signal on_death
+
+const SPEED := 60.0
 
 var health := 30
 
@@ -14,7 +14,7 @@ func _physics_process(_delta: float) -> void:
 
 	var dir := (player.global_position - global_position).normalized()
 	velocity = dir * SPEED
-	move_and_slide()
+	move_and_collide(velocity * _delta)
 
 
 func take_damage(amount: int) -> void:

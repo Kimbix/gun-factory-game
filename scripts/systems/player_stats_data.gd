d7a348658d8de659fe90a2aa37bf5c94478d8d4a
Kimbix <alemanhumberto06@gmail.com>
2026-07-20 17:47:52 -0400

    chore: Changed some values around

--- scripts/systems/player_stats_data.gd
+++ scripts/systems/player_stats_data.gd
@@ -2,7 +2,7 @@ class_name PlayerStatsData
 extends Resource
 
 @export var max_health: float = 100.0
-@export var move_speed: float = 200.0
+@export var move_speed: float = 75.0
 @export var tick_speed: float = 0.05
 @export var health_regen: float = 0.0
-@export var pickup_range: float = 80.0
+@export var pickup_range: float = 20.0

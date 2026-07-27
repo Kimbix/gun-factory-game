649ebce660fca27347d184d5a441c3a3df4c554b
Kimbix <alemanhumberto06@gmail.com>
2026-07-27 15:57:08 -0400

    refactor: Moved all logic back to base_enemy

--- objects/enemies/enemy_slime.gd
+++ objects/enemies/enemy_slime.gd
@@ -1,29 +1,2 @@
 class_name EnemySlime
 extends BaseEnemy
-
-signal on_death
-
-const SPEED := 20.0
-
-var health := 30
-var _dead: bool
-
-
-func _physics_process(_delta: float) -> void:
-	if player == null or not is_instance_valid(player):
-		return
-
-	var dir := (player.global_position - global_position).normalized()
-	velocity = dir * SPEED
-	move_and_collide(velocity * _delta)
-
-
-# TODO: This should not be on the slime's, should be base enemy's code
-func take_damage(amount: int) -> void:
-	if _dead:
-		return
-	health -= amount
-	if health <= 0:
-		_dead = true
-		on_death.emit()
-		queue_free()
--- scripts/entities/base_enemy.gd
+++ scripts/entities/base_enemy.gd
@@ -1,16 +1,40 @@
 class_name BaseEnemy
 extends CharacterBody2D
 
+## Set by [EnemyInfo] when being spawned by [GameSupervisor].
+signal on_death
+
 enum EnemyType { REGULAR, BOSS }
+
+const SPEED := 20.0
 
 @export var enemy_type: EnemyType = EnemyType.REGULAR
 
 var player: Node2D
 var game_world: Node
-
-## Set by [EnemyInfo] when being spawned by [GameSupervisor].
+var health := 30
 var xp_amount: int
+var _dead: bool
 
 
 func _ready() -> void:
 	add_to_group("enemy")
+
+
+func _physics_process(_delta: float) -> void:
+	if player == null or not is_instance_valid(player):
+		return
+
+	var dir := (player.global_position - global_position).normalized()
+	velocity = dir * SPEED
+	move_and_collide(velocity * _delta)
+
+
+func take_damage(amount: int) -> void:
+	if _dead:
+		return
+	health -= amount
+	if health <= 0:
+		_dead = true
+		on_death.emit()
+		queue_free()

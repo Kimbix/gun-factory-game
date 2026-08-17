0ce9fb6009d4325b9c3f84c752b285aff4570d9f
Kimbix <alemanhumberto06@gmail.com>
2026-08-17 14:38:11 -0400

    feat: Show small tooltip on hovering inventory

--- interfaces/components/building_inventory_entry.gd
+++ interfaces/components/building_inventory_entry.gd
@@ -7,3 +7,7 @@ extends Button
 func setup(stack: BuildingStack) -> void:
 	self.icon = stack.info.texture
 	count_label.text = "%sx" % stack.count
+
+	var tooltip_str := "%s" % stack.info.display_name
+	mouse_entered.connect(TooltipCanvas.show_tooltip.bind(tooltip_str))
+	mouse_exited.connect(TooltipCanvas.hide_tooltip)

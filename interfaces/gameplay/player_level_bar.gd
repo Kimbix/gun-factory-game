675efa19ac2f125f3ada6ed6c00602294182af11
Kimbix <alemanhumberto06@gmail.com>
2026-08-17 14:16:58 -0400

    feat: Created gold indicator in normal gameplay

--- interfaces/gameplay/player_level_bar.gd
+++ interfaces/gameplay/player_level_bar.gd
@@ -5,13 +5,16 @@ extends Control
 @export var fill_color: Color = Color(0.2, 0.6, 1.0)
 @export var _fill: ColorRect
 @export var _experience_label: Label
+@export var _current_gold: Label
 
 
 func setup(ls: LevelSystem) -> void:
 	_lvl(ls.level)
 	_xp_bar(ls.xp, ls.xp_to_next_level)
+	_gold_count(ls.gold)
 	ls.leveled_up.connect(_on_level_up)
 	ls.xp_changed.connect(_on_xp_changed)
+	ls.gold_changed.connect(_gold_count)
 
 
 func _on_level_up(lvl: int) -> void:
@@ -32,3 +35,7 @@ func _xp_bar(xp: int, limit: int) -> void:
 		return
 	var ratio: float = float(xp) / float(limit)
 	_fill.anchor_right = clampf(ratio, 0.0, 1.0)
+
+
+func _gold_count(gold: int) -> void:
+	_current_gold.text = "Gold: %s" % gold

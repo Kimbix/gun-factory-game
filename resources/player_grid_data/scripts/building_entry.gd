25c4ee3aa506a662b16cfc64a8456b6e10309240
Kimbix <alemanhumberto06@gmail.com>
2026-08-24 17:55:10 -0400

    refactor: Fixed typing in building_entry.gd

--- resources/player_grid_data/scripts/building_entry.gd
+++ resources/player_grid_data/scripts/building_entry.gd
@@ -2,6 +2,6 @@ class_name BuildingEntry
 extends Resource
 
 @export var position: Vector2i
-@export var rotation: int
+@export var rotation: FactoryBuilding.Rotation
 @export var info: GridComponentInfo
 @export var variables: Dictionary[StringName, Variant]
